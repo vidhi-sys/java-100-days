f76380404b1f39ce30a088b5f4c1a3e1974d2336
vidhi udasi <udasividhi18@gmail.com>
2024-09-26 19:24:55 +0530

    Update var_2.java

--- var_2.java
+++ var_2.java
@@ -1,3 +1,11 @@
+class test{
+    int [] arr = new int[3];// instance level array declaration
+    static int []arr_2 = new int[3];
+    test(){
+        System.out.println(arr_2);
+        System.out.println(arr_2[0]);
+    }
+}
 public class var_2 {
     public static void main(String[] args) {
         int x;
@@ -30,4 +38,13 @@ public class var_2 {
     int w=10;
 //LOCAL REFRENCE 
     int[]W= new int[3];
+        /*  uninitalised arrays
+        int [] arr = new int[];
+        test  c= new test();
+        System.out.println(c.arr[0]); error*/
+        test  c= new test();
+        System.out.println(c.arr);
+        System.out.println(c.arr[0]); // valid default value for int=0 printed
+// incase of static we can directly acess
+        // local level no default value =ERROR
     }}
