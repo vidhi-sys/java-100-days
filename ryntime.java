029591b1dd31254a07c7ce28edafbcce32d70b44
vidhi udasi <udasividhi18@gmail.com>
2024-10-13 22:06:11 +0530

    Add files via upload

---
+++ ryntime.java
@@ -0,0 +1,22 @@
+//WAP to implement run time polymorphism by applying dynamic dispatch method in method overriding
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+}}
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }}
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Cat meows");
+    }}
+public class ryntime {
+    public static void main(String[] args) {
+        Animal myDog = new Dog(); // Upcasting
+        Animal myCat = new Cat(); // Upcasting
+
+        // Dynamic method dispatch
+        myDog.sound(); // Calls Dog's sound()
+        myCat.sound(); // Calls Cat's sound()
+    }}
